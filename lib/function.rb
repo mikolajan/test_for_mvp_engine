1373650fd426e04d838f26c69b74b1515b078c87
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2021-02-09 21:31:31 +0300

    added method get_pair for module Function

--- lib/function.rb
+++ lib/function.rb
@@ -2,7 +2,20 @@ module Function
   SUM = 5.freeze
 
   def self.get_pairs(arr, sum = SUM)
+    result = []
 
+    while arr.any?
+      num = arr.shift
 
+      if arr.include?(sum - num)
+        arr.delete_at(arr.index(sum - num))
+
+        result << [num, sum - num]
+      else
+        arr.delete(num)
+      end
+    end
+
+    result
   end
 end
