8f4286b3e716143954b37b0d36bba954e2e71ca7
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2021-02-09 21:22:13 +0300

    added tests for Function.get_pair(arr, sum)

--- lib/function.rb
+++ lib/function.rb
@@ -1,5 +1,8 @@
 module Function
-  def self.get_pairs(arr, sum)
+  SUM = 5.freeze
+
+  def self.get_pairs(arr, sum = SUM)
+
 
   end
 end
--- spec/function_spec.rb
+++ spec/function_spec.rb
@@ -1,5 +1,21 @@
 require 'function'
 
 describe Function do
+  describe '#get_pairs' do
+    it 'returns [[3, 2], [5, 0]]' do
+      expect(Function.get_pairs([22, 3, 5, 0, 2, 2])).to eq [[3, 2], [5, 0]]
+    end
 
+    it 'returns [[-5, 10], [2, 3], [2, 3], [5, 0]]' do
+      expect(Function.get_pairs([-5, 33, 2, 2, 3, 5, 0, 10, 3])).to eq [[-5, 10], [2, 3], [2, 3], [5, 0]]
+    end
+
+    it 'returns [[5, 0], [5, 0], [5, 0]]' do
+      expect(Function.get_pairs([5, 5, 5, 0, 0, 0, 5])).to eq([[5, 0], [5, 0], [5, 0]])
+    end
+
+    it 'returns [[3, 3], [6, 0]]' do
+      expect(Function.get_pairs([3, 3, 6, 0], 6)).to eq([[3, 3], [6, 0]])
+    end
+  end
 end
