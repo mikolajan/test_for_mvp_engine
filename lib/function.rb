f552cdb1015d5e10a6d96c53bfa8b4e63df70b13
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2021-02-10 09:24:14 +0300

    refactoring #get_pairs(num, sum)

--- lib/function.rb
+++ lib/function.rb
@@ -1,18 +1,22 @@
 module Function
   SUM = 5.freeze
 
+  # Method groups numbers by sum of pairs.
+  # Each element of array has to be used only once.
   def self.get_pairs(arr, sum = SUM)
     result = []
 
     while arr.any?
       num = arr.shift
 
-      if arr.include?(sum - num)
-        arr.delete_at(arr.index(sum - num))
+      index_of_pair = arr.index(sum - num)
+
+      if index_of_pair.nil?
+        arr.delete(num)
+      else
+        arr.delete_at(index_of_pair)
 
         result << [num, sum - num]
-      else
-        arr.delete(num)
       end
     end
 
